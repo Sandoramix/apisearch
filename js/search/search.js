3ce1b6ba1096f8aaa8e5c03ace14d52ebc04cc9f
Sandoramix <sandoramix.dev@gmail.com>
2023-04-13 19:42:47 +0200

    added result count + paginator

--- js/search/search.js
+++ js/search/search.js
@@ -1,61 +1,148 @@
+// HEADER
 const CATEGORY_SELECT = document.getElementById(`category`);
 const QUERY_INPUT = document.getElementById(`query`);
-const RESULT_LIST = document.getElementById(`list`);
 
+// MAIN
+const TOTAL_COUNT_PARAGRAPH = document.getElementById(`total-count`);
+const RESULT_LIST = document.getElementById(`list`);
 
 const SEARCH_RESULT_TEMPLATE = document.getElementById(`search-result-item`);
 
+// FOOTER
+const PAGE_SWITCHER_DIV = document.getElementById(`page-switcher`);
+const CURRENT_PAGE_PARAGRAPH = document.getElementById(`current-page`);
+const TOTAL_PAGES_COUNT = document.getElementById(`total-pages-count`);
+const PREVIOUS_PAGE_BTN = document.getElementById(`page-switch-previous`);
+const NEXT_PAGE_BTN = document.getElementById(`page-switch-next`);
+const OFFSET_SWITCHER_SELECT = document.getElementById(`offset-switcher`);
+
+
 var { query: queryQuery, category: queryCategory } = getPageParams();
-queryQuery = queryQuery ?? "";
-queryCategory = queryCategory ?? "";
 
 var resultForgedElements = [];
 
 
-var currentIndex = 0;
-var offset = 20;
+var currentPage = 1;
+var totalPages = undefined;
+
+const offsetOptions = [5, 10, 25, 50];
+var pageOffset = 5;
+
 
 
 const loadQueryValues = () => {
 	CATEGORY_SELECT.value = queryCategory;
 	QUERY_INPUT.value = queryQuery;
 };
+const loadOffset = () => {
+	fillSelect(offsetOptions, OFFSET_SWITCHER_SELECT);
+	try {
+		const lsData = localStorage.getItem(LS.OFFSET);
+		const parsedNumber = Number.parseInt(lsData);
+		if (isNaN(parsedNumber)) throw 'Not A Number';
+		pageOffset = parsedNumber;
+	} catch (error) {
+		pageOffset = offsetOptions[0];
+	}
+};
+
+const updateCurrentPageParagraph = () => {
+	CURRENT_PAGE_PARAGRAPH.textContent = currentPage;
+};
+const updateOffset = () => {
+	let parsedOffset = undefined;
+	try {
+		parsedOffset = Number.parseInt(OFFSET_SWITCHER_SELECT.value);
+	} catch (error) {
+		parsedOffset = offsetOptions[0];
+	}
+	pageOffset = parsedOffset;
+	localStorage.setItem(LS.OFFSET, pageOffset.toString());
+
+	calculateTotalPages();
+	goToPage(1);
+};
+
+const showResults = (list) => {
+	RESULT_LIST.innerHTML = ``;
+	const currentIndex = (currentPage - 1) * pageOffset;
+	const chunk = list.slice(currentIndex, currentIndex + pageOffset);
+
+	chunk.forEach(el => {
+		RESULT_LIST.appendChild(el.cloneNode(true));
+	});
+	const size = resultForgedElements.length;
+	TOTAL_COUNT_PARAGRAPH.textContent = `${currentPage > 1 ? `Page ${currentPage} of` : `Found`} ${size} result${size === 1 ? `` : `s`}`;
+};
+
+
+const calculateTotalPages = () => {
+	totalPages = Math.ceil(resultForgedElements.length / pageOffset);
+	if (totalPages === 0) {
+		PAGE_SWITCHER_DIV.classList.toggle(`!flex`, false);
+		return;
+	}
+	PAGE_SWITCHER_DIV.classList.toggle(`!flex`, true);
+
+	TOTAL_PAGES_COUNT.textContent = totalPages;
+};
+
+
+function canGoToPage(page) {
+	return {
+		previousButton: (page > 1 && totalPages !== 0),
+		nextButton: (page < totalPages && totalPages !== 0),
+		result: (page >= 1 && page <= totalPages && totalPages !== 0)
+	};
+}
+const goToPage = (page = 1) => {
+
+	const check = canGoToPage(page);
+
+	PREVIOUS_PAGE_BTN.disabled = !check.previousButton;
+	NEXT_PAGE_BTN.disabled = !check.nextButton;
+	if (!check.result) {
+		return;
+	}
+	currentPage = page;
+
+	updateCurrentPageParagraph();
+	showResults(resultForgedElements);
+};
+
+// STARTING CALLING THE NEEDED FUNCTIONS
+
+PREVIOUS_PAGE_BTN.addEventListener('click', () => {
+	goToPage(currentPage - 1);
+});
+NEXT_PAGE_BTN.addEventListener('click', () => {
+	goToPage(currentPage + 1);
+});
+OFFSET_SWITCHER_SELECT.addEventListener(`input`, () => {
+	updateOffset();
+});
+
+
+loadOffset();
 loadQueryValues();
 
 getAllCategories()
 	.then(data => {
-		fillCategories(data, CATEGORY_SELECT);
+		fillSelect(data, CATEGORY_SELECT);
 		loadQueryValues();
 	})
 	.catch(console.error);
-
-
-function showResults(list) {
-	RESULT_LIST.innerHTML = ``;
-	list.slice(currentIndex, currentIndex + offset).forEach(el => RESULT_LIST.appendChild(el));
-}
 
 
 getEntries(queryQuery, queryCategory)
 	.then(data => {
 		resultForgedElements = data.map(api => {
 			const copy = document.importNode(SEARCH_RESULT_TEMPLATE.content, true);
-			copy.querySelectorAll('[data-key]').forEach(element => {
-				const key = element.getAttribute(`data-key`);
-				if (key.toUpperCase() === 'URL') {
-					element.href = api.Link;
-					return;
-				}
-				let fixedText = api[key];
-				fixedText = fixedText == '' ? 'N/A' : fixedText;
-				element.textContent = fixedText;
-			});
+			const selectorElements = copy.querySelectorAll('[data-key]');
+			generateApiItem(api, selectorElements);
 
 			return copy;
 		});
-		console.log({ resultForgedElements });
-		return resultForgedElements;
-	})
-	.then(forgedElements => {
-		showResults(forgedElements);
+		calculateTotalPages();
+		goToPage(1);
 	});
