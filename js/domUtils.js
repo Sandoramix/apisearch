c6095b461d7eff28cf84369ad1e418ebc6267dc9
Sandoramix <sandoramix.dev@gmail.com>
2023-04-13 19:44:17 +0200

    refactor + utility functions

--- js/domUtils.js
+++ js/domUtils.js
@@ -1,9 +1,24 @@
-function fillCategories(list, selectContainer) {
-	list.forEach(category => {
+
+
+function fillSelect(list, selectContainer) {
+	list.forEach(value => {
 		const option = document.createElement(`option`);
-		option.value = category;
-		option.innerText = category;
+		option.value = value;
+		option.innerText = value;
 		selectContainer.append(option);
 	});
 	selectContainer.disabled = false;
+};
+
+function generateApiItem(apiObject, selectorItems) {
+	selectorItems.forEach(element => {
+		const key = element.getAttribute(`data-key`);
+		if (key.toUpperCase() === 'URL') {
+			element.href = apiObject.Link;
+			return;
+		}
+		let fixedText = apiObject[key];
+		fixedText = fixedText == '' ? 'N/A' : fixedText;
+		element.textContent = fixedText;
+	});
 };
